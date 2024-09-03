cf8a1560ad2c7e338b8f0c905920a9c0b99fc9b1
Francis Duvivier <francis.duvivier@persgroep.net>
2024-09-03 21:17:19 +0200

    move api test code to test_api.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 
 import accel
 import rgb
-import buttons
+
 
 
 # Setup RGB
@@ -109,30 +109,9 @@ def update_block_position():
 
         block_x, block_y = new_x, new_y
 
-def test_image():
-    data = [0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff]
-    rgb.clear()
-    rgb.image(data, pos=(12, 0), size=(8, 8))
-
-def clear_display(button_is_down, button_name):
-    print('BUTTON '+button_name+' Callback! down: '+ str(button_is_down))
-    if button_is_down:
-        rgb.clear()
-        pass
-
-def setup_button_callback():
-     buttons.register(buttons.BTN_A, lambda down: clear_display(down, 'A'))
-     buttons.register(buttons.BTN_B, lambda down: clear_display(down, 'B'))
-     buttons.register(buttons.BTN_UP, lambda down: clear_display(down, 'UP'))
-     buttons.register(buttons.BTN_DOWN, lambda down: clear_display(down, 'DOWN'))
-     buttons.register(buttons.BTN_LEFT, lambda down: clear_display(down, 'LEFT'))
-     buttons.register(buttons.BTN_RIGHT, lambda down: clear_display(down, 'RIGHT'))
-
 def main():
     log("Entering main function")
-    test_image()
     time.sleep(0.5)
-    setup_button_callback()
     fill(50,100,50)
     write_kolab()
     time.sleep(0.5)
---
+++ test_api.py
@@ -0,0 +1,47 @@
+import buttons
+import time
+import rgb
+import accel
+
+def clear_display(button_is_down, button_name):
+    print('BUTTON '+button_name+' Callback! down: '+ str(button_is_down))
+    if button_is_down:
+        rgb.background((200, 200, 200))
+        time.sleep(0.2)
+        rgb.clear()
+        pass
+
+def setup_button_callback():
+    buttons.register(buttons.BTN_A, lambda down: clear_display(down, 'A'))
+    buttons.register(buttons.BTN_B, lambda down: clear_display(down, 'B'))
+    buttons.register(buttons.BTN_UP, lambda down: clear_display(down, 'UP'))
+    buttons.register(buttons.BTN_DOWN, lambda down: clear_display(down, 'DOWN'))
+    buttons.register(buttons.BTN_LEFT, lambda down: clear_display(down, 'LEFT'))
+    buttons.register(buttons.BTN_RIGHT, lambda down: clear_display(down, 'RIGHT'))
+
+
+def test_image():
+    data = [0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0x000000ff, 0x000000ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff, 0xffbf00ff]
+    rgb.clear()
+    rgb.image(data, pos=(12, 0), size=(8, 8))
+
+def print_accel():
+    print("accel"+str(accel.get_xyz()))
+
+def main():
+    print("Entering main function")
+    accel.init()
+    setup_button_callback()
+    while True:
+        print_accel()
+        test_image()
+        time.sleep(2)
+        rgb.background((0,100,0))
+        time.sleep(2)
+
+
+
+
+if __name__ == "__main__":
+    print("Script started")
+    main()
